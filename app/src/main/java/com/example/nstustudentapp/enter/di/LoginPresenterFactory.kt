0d14723d2ef0ae81fda1bc63fb545d176b6ec1d2
tmiiin <English980@mail.ru>
2021-01-08 21:20:35 +0700

    Delete unnecessary imports

--- app/src/main/java/com/example/nstustudentapp/enter/di/LoginPresenterFactory.kt
+++ app/src/main/java/com/example/nstustudentapp/enter/di/LoginPresenterFactory.kt
@@ -1,12 +1,9 @@
 package com.example.nstustudentapp.enter.di
 
-import android.content.SharedPreferences
 import com.example.nstustudentapp.enter.data.datasource.LoginDataSourceImpl
 import com.example.nstustudentapp.enter.data.repository.LoginRepositoryImpl
-import com.example.nstustudentapp.enter.domain.GetStusentUseCase
 import com.example.nstustudentapp.enter.domain.LoginUseCase
 import com.example.nstustudentapp.enter.presentation.AuthPresenter
-import com.example.nstustudentapp.enter.presentation.ScreenSaverPresenter
 
 object LoginPresenterFactory {
 
